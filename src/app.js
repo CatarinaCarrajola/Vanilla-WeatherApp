d70cf3e20ad1d4d8db10e704d0e2d5885ea4e9f6
CatarinaCarrajola <catarinadasilvac+github@gmail.com>
2021-08-23 14:52:08 +0200

    Icon now updates from API

--- src/app.js
+++ src/app.js
@@ -28,18 +28,23 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "43da0ce795180b550b43f6866a3bc1ab";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=oslo&appid=${apiKey}&units=metric`;
+let city = "Oslo";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
-
-//let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-//https://api.openweathermap.org/data/2.5/weather?q=oslo&appid=3da0ce795180b550b43f6866a3bc1ab&units=metric;
